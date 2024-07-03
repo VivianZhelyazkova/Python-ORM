ae6b9955764fe82b8a937076627c72c99b3e4b1f
Viviancat666 <vivian9069@gmail.com>
2024-07-03 21:47:44 +0300

    Create caller.py

---
+++ 05/caller.py
@@ -0,0 +1,52 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+from main_app.models import Pet, Artifact, Location
+
+
+# Import your models here
+
+# Create queries within functions
+
+def create_pet(name: str, species: str):
+    pet = Pet.objects.create(name=name, species=species)
+    return f"{pet.name} is a very cute {pet.species}!"
+
+
+def create_artifact(name: str, origin: str, age: int, description: str, is_magical: bool):
+    artifact = Artifact.objects.create(name=name, origin=origin, age=age, description=description,
+                                       is_magical=is_magical)
+    return f"The artifact {artifact.name} is {artifact.age} years old!"
+
+
+def rename_artifact(artifact: Artifact, new_name: str):
+    if artifact.is_magical and artifact.age > 250:
+        artifact.name = new_name
+        artifact.save()
+
+
+def delete_all_artifacts():
+    Artifact.objects.all().delete()
+
+
+def show_all_locations():
+    locations = Location.objects.all.order_by('-id')
+    return "\n".join(str(l) for l in locations)
+
+
+def new_capital():
+    location = Location.objects.first()
+    location.is_capital = True
+    location.save()
+
+
+def get_capitals():
+    return Location.objects.filter(is_capital=True).values("name")
+
+
+def delete_first_location():
+    Location.objects.first().delete()
